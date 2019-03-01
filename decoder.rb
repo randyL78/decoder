e521b039ed342ab46e44414ccf5631a0010ebc2e
Randy's Mac <randydavidl78@gmail.com>
2019-03-01 17:31:48 -0500

    add A-I conversions, because I can't wait

--- decoder.rb
+++ decoder.rb
@@ -1,5 +1,14 @@
 # Define a method that takes an array of numbers to decode
 KEY = {
+  1 => 'A',
+  2 => 'A',
+  3 => 'C',
+  4 => 'D',
+  5 => 'E',
+  6 => 'F',
+  7 => 'G',
+  8 => 'H',
+  9 => 'I',
 }
 
 # Define a method that takes an array of numbers to decode.
